fee23fa3bc2403b6663a3c8fd6a4de9dd10e500a
marshl <leeism@live.com.au>
2014-02-27 16:28:53 +1100

    Cleaned up the position control script Clobbered some meta files along the way

--- Assets/Scripts/NetworkPositionControl.cs
+++ Assets/Scripts/NetworkPositionControl.cs
@@ -4,40 +4,41 @@ using System.Collections.Generic;
 
 public class NetworkPositionControl : MonoBehaviour
 {
+	/// A position and rotation point sent over the network
 	private struct DataPoint
 	{
-		/*public DataPoint( Vector3 _pos, Quaternion _forward, double _timeStamp )
-		{
-			this.position = _pos;
-			this.rotation = _rotation;
-			this.timeStamp = _timeStamp;
-		}
-*/
 		public Vector3 position;
-		//public Vector3 forward;
 		public Quaternion rotation;
 		public double timeStamp;
 	};
-	
+
+	// Two points of data to be used to guess the future position
 	private DataPoint olderData;
 	private DataPoint newerData;
 
+	// Precalculated variables that only have to be set every time a packet is received
 	private double timeDiff = 0.0f;
 	private float distTravelled = 0.0f;
-	//private float angleChange = 0.0f;
 	private Vector3 axis = Vector3.up;
 	private float twistChange;
 
+	/// <summary>
+	/// Will this object lerp its position towards the guesstimate?
+	/// </summary>
 	public bool lerp;
+
+	/// <summary>
+	/// If it is, how fast will it move
+	/// </summary>
 	public float lerpRate;
 
-	   
+	// Unity Callback: Do not change signature
 	private void Awake()
 	{
+		// Various reasons why this script is not needed
 		if ( this.networkView == null
 		|| networkView.isMine == true
-		|| ( Network.isClient == false
-		  && Network.isServer == false ) )
+		|| ( Network.isClient == false && Network.isServer == false ) )
 		{
 			this.enabled = false;
 			return;
@@ -53,49 +54,38 @@ public class NetworkPositionControl : MonoBehaviour
 		{
 			Vector3 pos = this.transform.localPosition;
 			Quaternion rot = this.transform.localRotation;
-			//Vector3 forward = this.transform.forward;
 			_stream.Serialize( ref pos );
 			_stream.Serialize( ref rot );
-			//_stream.Serialize( ref forward );
 		}
 		else if ( _stream.isReading == true )
 		{
 			Vector3 pos = Vector3.zero;
 			Quaternion rot = Quaternion.identity;
-			//Vector3 forward = Vector3.zero;
 			_stream.Serialize( ref pos );
 			_stream.Serialize( ref rot );
-			//_stream.Serialize( ref forward );
 
-			this.SerialiseData( pos, rot, _info.timestamp );
-			//this.SerialiseData( pos, forward, _info.timestamp );
+			this.StoreData( pos, rot, _info.timestamp );
 		}	
 	}
 	 
-	public void SerialiseData( Vector3 _pos, 
-	                         //Vector3 _forward, 
-	                           Quaternion _rot, 
-	                          double _time )
+	/// <summary>
+	/// Stores the data that is sent over the network (kept separate for testing purposes)
+	/// </summary>
+	/// <param name="_pos">The last known position</param>
+	/// <param name="_rot">The last known rotation</param>
+	/// <param name="_time">The time at which the pos and rot were sent</param>
+	public void StoreData( Vector3 _pos, Quaternion _rot, double _time )
 	{
-		//this.olderData.forward = this.newerData.forward;
 		this.olderData.rotation = this.newerData.rotation;
 		this.olderData.position = this.newerData.position;
 		this.olderData.timeStamp = this.newerData.timeStamp;
 
-		//this.newerData.forward = _forward;
 		this.newerData.rotation = _rot;
 		this.newerData.position = _pos;
 		this.newerData.timeStamp = _time;
 		
 		this.timeDiff = this.newerData.timeStamp - this.olderData.timeStamp;
-		//Debug.Log( "TimeDiff: " + timeDiff );
-		 
-		/*if ( this.newerData.forward != this.olderData.forward )
-		{
-			this.axis = Vector3.Cross( this.olderData.forward, this.newerData.forward );
-			this.angleChange = Vector3.Angle( this.olderData.forward, this.newerData.forward );
-		}
-		*/
+
 		Vector3 vectorTravelled = this.newerData.position - this.olderData.position;
 		this.distTravelled = vectorTravelled.magnitude;
 
@@ -109,10 +99,15 @@ public class NetworkPositionControl : MonoBehaviour
 
 	private void Update()
 	{
-		this.GuesstimatePosition( Network.time );
+		// This is separate to facilitate local testing
+		this.TransformLerp( Network.time );
 	}
 
-	public void GuesstimatePosition( double _currentTime )
+	/// <summary>
+	/// Gradually moves this position towards where it is thought it should be at the current time
+	/// </summary>
+	/// <param name="_currentTime">The time</param>
+	public void TransformLerp( double _currentTime )
 	{
 		// We need two or more points for this to work
 		//TODO: A basic move using only one point (LM:21/02/14)
@@ -124,30 +119,11 @@ public class NetworkPositionControl : MonoBehaviour
 
 		double timeSince = _currentTime - this.newerData.timeStamp;
 		float multiplier = (float)(timeSince / timeDiff);
-		//Debug.Log( "OldTime:" + this.olderData.timeStamp + " NewTime:" + this.newerData.timeStamp + "Diff:" + this.timeDiff + " Since:" + timeSince + " Multiplier: " + multiplier );
-		//Vector3 forwardGuess = Quaternion.AngleAxis( this.angleChange * multiplier, this.axis ) * this.newerData.forward;
-		
-		//Quaternion guessRotation = Quaternion.Lerp( this.olderData.rotation, this.newerData.rotation, 1.0f + multiplier );
-		//Quaternion guessRotation = this.olderData.rotation * this.newerData.rotation;
-		//Vector3 oldEuler = this.olderData.rotation.eulerAngles;
-		//Vector3 newEuler = this.newerData.rotation.eulerAngles;
 
-		//Quaternion increment = Quaternion.Lerp( this.olderData.rotation, this.newerData.rotation, multiplier );
+		Quaternion targetRotation = this.newerData.rotation * Quaternion.AngleAxis( 
+			 Quaternion.Angle( this.olderData.rotation, this.newerData.rotation ) * multiplier, this.axis
+	    );
 
-
-		//Vector3 rotEuler = newEuler + ( newEuler - oldEuler ) * multiplier;
-		//Quaternion.Lerp(
-		//TODO: This should probably lerp towards the target position/rotation
-		//TODO: Derp, this doesn't handle twist
-		//this.transform.LookAt( this.transform.position + forwardGuess );
-		//Debug.Log( guessRotation );
-
-		//this.transform.rotation = increment * this.newerData.rotation;
-		Quaternion targetRotation = this.newerData.rotation * Quaternion.AngleAxis( Quaternion.Angle( this.olderData.rotation, this.newerData.rotation ) * multiplier, this.axis );
-
-		//this.transform.localRotation = guessRotation;
-		//this.transform.localRotation = Quaternion.Euler( rotEuler );
-		//this.transform.localPosition = this.newerData.position;
 		Vector3 targetPosition = this.newerData.position + this.transform.forward * multiplier * this.distTravelled;
 
 		if ( this.lerp == true )
@@ -160,6 +136,5 @@ public class NetworkPositionControl : MonoBehaviour
 			this.transform.localPosition = targetPosition;
 			this.transform.localRotation = targetRotation;
 		}
-		//this.transform.position = this.newerData.position + forwardGuess * multiplier * this.distTravelled;
 	}
 }
--- Assets/Scripts/Test/NetworkPositionTest.cs
+++ Assets/Scripts/Test/NetworkPositionTest.cs
@@ -17,11 +17,11 @@ public class NetworkPositionTest : MonoBehaviour
 		if ( this.timer >= this.latency )
 		{
 			this.timer = 0.0f;
-			this.other.SerialiseData( this.transform.position, this.transform.rotation, Time.realtimeSinceStartup );
+			this.other.StoreData( this.transform.position, this.transform.rotation, Time.realtimeSinceStartup );
 			//this.other.SerialiseData( this.transform.position, this.transform.forward, Time.realtimeSinceStartup );
 		}
 		 
-		this.other.GuesstimatePosition( Time.realtimeSinceStartup );
+		this.other.TransformLerp( Time.realtimeSinceStartup );
 		this.transform.Rotate( this.transform.up, this.turnSpeed * Time.deltaTime );
 		this.transform.Rotate( this.transform.forward, this.twistSpeed * Time.deltaTime );
 		this.transform.position += this.transform.forward * Time.deltaTime * this.moveSpeed;
